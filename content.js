84fc41038ffce7bb935446f71cbce7fca1f29101
SamHwang1990 <samhwang1990@gmail.com>
2015-09-24 20:12:37 +0800

    in second process

---
+++ content.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2015/9/24.
+ */
+
+module.export = 'It works from content.js!';
---
+++ entry.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2015/9/24.
+ */
+
+document.write(require('./content.js'));
